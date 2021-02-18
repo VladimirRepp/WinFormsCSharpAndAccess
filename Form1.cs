4ba2b49584a9bfd8945e069615fecd997ce1598e
FPS.FirePlanetStudio <57412538+VladimirRepp@users.noreply.github.com>
2021-02-18 11:26:50 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ExampleCS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void обПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Пример работы на WinForms C# и ACCESS.","Внимание!");
+        }
+
+        private void button_download_Click(object sender, EventArgs e)
+        {
+            //Создаем соеденение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";//строка соеденения
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);//создаем соеденение
+
+            //Выполянем запрос к БД
+            dbConnection.Open();//открываем соеденение
+            string query = "SELECT * FROM table_name";//строка запроса
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);//команда
+            OleDbDataReader dbReader = dbCommand.ExecuteReader();//считываем данные
+
+            //Проверяем данные
+            if (dbReader.HasRows == false)
+            {
+                MessageBox.Show("Данные не найдены!","Ошибка!");
+            }
+            else
+            {
+                //Запишем данные в таблицу формы
+                while (dbReader.Read())
+                {
+                    //Выводим данные
+                    //dataGridView1.Rows.Add(dbReader["id"], dbReader["Name"], dbReader["Cost"], dbReader["Quantity"]);
+                }
+            }
+
+            //Закрываем соеденение с БД
+            dbReader.Close();
+            dbConnection.Close();
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            //Проверим количество выбранных строк
+            if(dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!","Внимание!");
+                return;
+            }
+
+            //Запомним выбранную строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            //Проверим данные в таблицы
+            if(dataGridView1.Rows[index].Cells[0].Value == null ||
+                dataGridView1.Rows[index].Cells[1].Value == null ||
+                dataGridView1.Rows[index].Cells[2].Value == null ||
+                dataGridView1.Rows[index].Cells[3].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!","Внимание!");
+                return;
+            }
+
+            //Считаем данные
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            string name = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            string cost = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            string quantity = dataGridView1.Rows[index].Cells[3].Value.ToString();
+
+            //Создаем соеденение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";//строка соеденения
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);//создаем соеденение
+
+            //Выполянем запрос к БД
+            dbConnection.Open();//открываем соеденение
+            string query = "INSERT INTO table_name VALUES (" + id + ", '" + name + "', " + cost + ", " + quantity + ")";//строка запроса
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);//команда
+
+            //Выполняем запрос
+            if (dbCommand.ExecuteNonQuery() != 1)
+                MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+            else
+                MessageBox.Show("Данные добавлены!","Внимание!");
+
+            //Закрываем соеденение с БД
+            dbConnection.Close();
+        }
+
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            //Проверим количество выбранных строк
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимание!");
+                return;
+            }
+
+            //Запомним выбранную строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            //Проверим данные в таблицы
+            if (dataGridView1.Rows[index].Cells[0].Value == null ||
+                dataGridView1.Rows[index].Cells[1].Value == null ||
+                dataGridView1.Rows[index].Cells[2].Value == null ||
+                dataGridView1.Rows[index].Cells[3].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимание!");
+                return;
+            }
+
+            //Считаем данные
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            string name = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            string cost = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            string quantity = dataGridView1.Rows[index].Cells[3].Value.ToString();
+
+            //Создаем соеденение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";//строка соеденения
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);//создаем соеденение
+
+            //Выполянем запрос к БД
+            dbConnection.Open();//открываем соеденение
+            string query = "UPDATE table_name SET Name = '" + name + "',Cost=" + cost + ",Quantity=" + quantity + " WHERE id = " + id;//строка запроса
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);//команда
+
+            //Выполняем запрос
+            if (dbCommand.ExecuteNonQuery() != 1)
+                MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+            else
+            {
+                MessageBox.Show("Данные изменены!", "Внимание!");
+            }
+
+            //Закрываем соеденение с БД
+            dbConnection.Close();
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            //Проверим количество выбранных строк
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимание!");
+                return;
+            }
+
+            //Запомним выбранную строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            //Проверим данные в таблицы
+            if (dataGridView1.Rows[index].Cells[0].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимание!");
+                return;
+            }
+
+            //Считаем данные
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+
+            //Создаем соеденение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";//строка соеденения
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);//создаем соеденение
+
+            //Выполянем запрос к БД
+            dbConnection.Open();//открываем соеденение
+            string query = "DELETE FROM table_name WHERE id = " + id;//строка запроса
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);//команда
+
+            //Выполняем запрос
+            if (dbCommand.ExecuteNonQuery() != 1)
+                MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+            else
+            {
+                MessageBox.Show("Данные удалены!", "Внимание!");
+                //Удаляем данные из таблицы в форме
+                dataGridView1.Rows.RemoveAt(index);
+            }
+
+            //Закрываем соеденение с БД
+            dbConnection.Close();
+        }
+    }
+}
